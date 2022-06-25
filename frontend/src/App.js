06a43240c075d07fdc4a7ae52d440e76c8574161
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-06-25 21:34:06 +0300

    Added Button component

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const goalList = [
+    { id: "1", text: "bla" },
+    { id: "2", text: "blaa" },
+    { id: "3", text: "blaaa" }
+  ];
+
+  return (
+    <div className="course-goals">
+      <h1>bla</h1>
+    </div>
+  );
+}
+
+export default App;
